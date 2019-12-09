92278be6ac273127b3a813a31ac2dd7bbc7ba3a2
mykindofscum <alvinkchan99@gmail.com>
2019-12-08 17:55:52 -0800

    add Meta.js, refactor components and pages

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
+import Meta from "../components/Meta";
 
 class Pages extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>
